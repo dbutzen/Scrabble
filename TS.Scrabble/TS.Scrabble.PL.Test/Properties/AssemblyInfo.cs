16453c3d507c1e13f66e58a71a8444db6a3dc62e
Andrew Linzmeier <300050292@fvtc.edu>
2021-02-12 16:21:34 -0600

    adds pl test, utGame

---
+++ TS.Scrabble/TS.Scrabble.PL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TS.Scrabble.PL.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TS.Scrabble.PL.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("869b6c41-b140-435c-9c78-3918bbc6e5be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TS.Scrabble/TS.Scrabble.PL.Test/utGame.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TS.Scrabble.PL.Test
+{
+    [TestClass]
+    public class utGame
+    {
+        protected ScrabbleEntities dc;
+        protected DbContextTransaction transaction;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dc = new ScrabbleEntities();
+            transaction = dc.Database.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            transaction.Rollback();
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(4, dc.tblGames.Count());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            tblGame row = new tblGame();
+            row.Id = -1;
+            row.Name = "Dog";
+            row.Password = "dog";
+            row.GameState = "";
+            row.DatePlayed = DateTime.Now;
+
+            dc.tblGames.Add(row);
+
+            int results = dc.SaveChanges();
+
+            Assert.IsTrue(results > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            InsertTest();
+
+            tblGame row = dc.tblGames.FirstOrDefault(g => g.Id == -1);
+
+            int results = 0;
+
+            if(row != null)
+            {
+                row.Name = "Cat";
+                results = dc.SaveChanges();
+            }
+
+            Assert.IsTrue(results > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            InsertTest();
+
+            tblGame row = dc.tblGames.FirstOrDefault(g => g.Id == -1);
+
+            int results = 0;
+
+            if (row != null)
+            {
+                dc.tblGames.Remove(row);
+                results = dc.SaveChanges();
+            }
+
+            Assert.IsTrue(results > 0);
+        }
+    }
+}
