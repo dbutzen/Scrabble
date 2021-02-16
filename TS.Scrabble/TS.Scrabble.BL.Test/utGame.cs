8e584265d8b22b979e6fee38d9a811fd8d5f471b
Keir Dvorachek <200004248@fvtc.edu>
2021-02-16 17:13:50 -0600

    Add BL.Test, update UserManager and Model.

---
+++ TS.Scrabble/TS.Scrabble.BL.Test/utGame.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TS.Scrabble.BL.Models;
+
+namespace TS.Scrabble.BL.Test
+{
+    [TestClass]
+    public class utGame
+    {
+        [TestMethod]
+        public void InsertTest()
+        {
+            Assert.IsTrue(GameManager.Insert(new Game { Id = -1, DatePlayed = DateTime.Now, Name = "Turtle", GameState = "", Password = "pass" }, true) > 0);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Game game = GameManager.LoadById(1);
+            game.Name = "Turtle";
+            Assert.IsTrue(GameManager.Update(game, true) > 0);
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Game game = GameManager.LoadById(1); //Will not work as is due to UserGame JunctionTable.
+            Assert.IsTrue(GameManager.Delete(game, true) > 0);
+        }
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(4, GameManager.Load().Count);
+        }
+    }
+}
---
+++ TS.Scrabble/TS.Scrabble.BL.Test/utUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TS.Scrabble.BL.Models;
+
+namespace TS.Scrabble.BL.Test
+{
+    [TestClass]
+    public class utUser
+    {
+        [TestMethod]
+        public void InsertTest()
+        {
+            Assert.IsTrue(UserManager.Insert(new User {FirstName = "Bob", LastName = "Bob", Email = "Bob@bob.com", Password = "Bob", Losses = 0, Wins = 0, Score = 0, CreationDate = DateTime.Now, Username = "Bobby"}, true) > 0);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            User user = UserManager.LoadById(1);
+            user.FirstName = "Turtle";
+            Assert.IsTrue(UserManager.Update(user, true) > 0);
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+            User user = UserManager.LoadById(1); //Will not work as is due to UserGame JunctionTable.
+            Assert.IsTrue(UserManager.Delete(user, true) > 0);
+        }
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(4, UserManager.Load().Count);
+        }
+        [TestMethod]
+        public void LoadByGameIdTest()
+        {
+            Assert.AreEqual(4, UserManager.Load(2).Count);
+        }
+    }
+}
