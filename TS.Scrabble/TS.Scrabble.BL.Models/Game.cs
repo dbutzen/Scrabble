f777a03a602ee0611e14c87ea3bb4c9be6e6c4c4
Andrew Linzmeier <300050292@fvtc.edu>
2021-03-03 16:38:25 -0600

    adds display names

--- TS.Scrabble/TS.Scrabble.BL.Models/Game.cs
+++ TS.Scrabble/TS.Scrabble.BL.Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace TS.Scrabble.BL.Models
         public Bag gameBag { get; set; }
 
         public int Id { get; set; }
+        [DisplayName("Date Played")]
         public DateTime DatePlayed { get; set; }
         public string GameState { get; set; }
         public string Name { get; set; }
--- TS.Scrabble/TS.Scrabble.BL.Models/User.cs
+++ TS.Scrabble/TS.Scrabble.BL.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,15 @@ namespace TS.Scrabble.BL.Models
     public class User
     {
         public int Id { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         public string Username { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
         public int Score { get; set; }
+        [DisplayName("User since")]
         public DateTime CreationDate { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
