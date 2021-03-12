324412dcceb1b2d19cd76273d9b5af73c8903b9c
Daniel Butzen <300057079@fvtc.edu>
2021-03-11 23:02:46 -0500

    Added getValue method to get the numeric value of a letter

--- TS.Scrabble/TS.Scrabble.MVCUI.2/Scripts/game.js
+++ TS.Scrabble/TS.Scrabble.MVCUI.2/Scripts/game.js
@@ -430,14 +430,74 @@ function BoardClicked(id) {
     var first = getNumberId(firstLetter);
     var secondLetter = id.charAt(2);
     var second = getNumberId(secondLetter);
-    gameArray[first, second].Tile.Letter = "";
-    gameArray[first, second].Tile.Value = "";
+    gameArray[first, second].Tile.Letter = document.getElementById(id).textContent; 
+    gameArray[first, second].Tile.Value = getValue(gameArray[first, second].Tile.Letter);
     gameArray[first, second].PlacedThisTurn = true;
 
     //Removes tile from your player hand array.
     piece.remove();
     var tile = parseInt(id.substring(10));
     players[0].hand.splice(tile, 1);
+}
+
+function getValue(letter){
+    switch (letter) {
+        case "A":
+            return 1;
+        case "B":
+            return 3;
+        case "C":
+            return 3;
+        case "D":
+            return 2;
+        case "E":
+            return 1;
+        case "F":
+            return 4;
+        case "G":
+            return 2;
+        case "H":
+            return 4;
+        case "I":
+            return 1;
+        case "J":
+            return 8;
+        case "K":
+            return 5;
+        case "L":
+            return 1;
+        case "M":
+            return 3;
+        case "N":
+            return 1;
+        case "O":
+            return 1;
+        case "P":
+            return 3;
+        case "Q":
+            return 10;
+        case "R":
+            return 1;
+        case "S":
+            return 1;
+        case "T":
+            return 1;
+        case "U":
+            return 1;
+        case "V":
+            return 4;
+        case "W":
+            return 4;
+        case "X":
+            return 8;
+        case "Y":
+            return 4;
+        case "Z":
+            return 10;
+        default:
+            return 0;
+
+    }
 }
 
 function getNumberId(letter) {
