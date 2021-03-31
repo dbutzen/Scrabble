e23bbd462c4faa542151e28e2fac3cf49e5c8895
Andrew Linzmeier <300050292@fvtc.edu>
2021-03-31 16:46:47 -0500

    obtains connection id and displays on screen

---
+++ TS.Scrabble/TS.Scrabble.MVCUI.2/Scripts/game-online.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var gameHub = $.connection.gameHub;
+
+    gameHub.client.showClientId = function (id) {
+        alert(id);
+    }
+
+    $.connection.hub.start().done(function () {
+        numPlayers += 1;
+        $('#clientId').click(function () {
+            gameHub.server.getClientId($.connection.hub.id)
+        });
+    });
+    
+});
